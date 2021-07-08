1c8c51b9939a9424d51c1445f8e2e1df27ffbf5e
橙汁 <1327085656@qq.com>
2021-07-08 20:13:48 +0800

    Main添加

--- src/view/Main.java
+++ src/view/Main.java
@@ -42,7 +42,7 @@ public class Main extends JFrame {
         menuItem6 = new JMenuItem();
         panel1 = new JPanel();
         scrollPane1 = new JScrollPane();
-        table1 = new JTable(information,subject);
+        table1 = new JTable();
 
         //======== this ========
         setForeground(SystemColor.menu);
@@ -122,12 +122,13 @@ public class Main extends JFrame {
 
         //======== panel1 ========
         {
-            panel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder
-            ( 0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border
-            .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt
-            . Color .red ) ,panel1. getBorder () ) ); panel1. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void
-            propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
-            ;} } );
+            panel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new
+            javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax
+            . swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java
+            . awt .Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 ) ,java . awt
+            . Color .red ) ,panel1. getBorder () ) ); panel1. addPropertyChangeListener( new java. beans .
+            PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062or\u0064er" .
+            equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
             panel1.setLayout(new BorderLayout());
 
             //======== scrollPane1 ========
